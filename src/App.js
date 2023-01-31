709628a86daf202b1bb8d1a377b042715f80f73b
kilaladashina@gmail.com <kilaladashina@gmail.com>
2023-01-31 14:58:56 +0300

    Update meta tag content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
